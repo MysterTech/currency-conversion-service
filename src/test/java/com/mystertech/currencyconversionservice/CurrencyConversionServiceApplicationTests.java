9a65f7ccbae31d1c79ebd504e4b4c18c5775962a
MysterTech <sanjay.krishna@MysterTech.com>
2020-10-28 11:01:56 +0530

    bootstrap from spring initializer

---
+++ src/test/java/com/mystertech/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystertech.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
